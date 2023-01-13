d783eb002e662d05312833994be215e1907fa401
ycp <820363773@qq.com>
2023-01-13 16:58:06 +0800

    update

---
+++ php/code/creative_mode/simple_factory/ConcreteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\creative_mode\simple_factory;
+
+class ConcreteFactory
+{
+    /**
+     * @var string[]
+     */
+    private $typeList;
+
+    public function __construct()
+    {
+        $this->typeList = [
+            'bicycle' => __NAMESPACE__ . '\Bicycle',
+            'other' => __NAMESPACE__ . '\scooter',
+        ];
+    }
+
+    public function createVehicle($type)
+    {
+        if(!array_key_exists($type, $this->typeList)){
+            throw new \InvalidArgumentException("$type");
+        }
+
+    }
+}
