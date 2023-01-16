22e0ec3771805722388270fe793c7ec8e3758e83
ycp <820363773@qq.com>
2023-01-16 21:44:04 +0800

    add 责任链模式

---
+++ php/code/Behavioral/ChainOfResponsibility/Responsible/FastStorage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Behavioral\ChainOfResponsibility\Responsible;
+
+use App\Behavioral\ChainOfResponsibility\Handler;
+use App\Behavioral\ChainOfResponsibility\Request;
+
+class FastStorage extends Handler
+{
+    private $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    protected function processing(Request $request)
+    {
+        echo 'fast';
+        if('get' == $request->verb){
+            if (array_key_exists($request->key, $this->data)) {
+                $request->response = $this->data[$request->key];
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ php/code/Behavioral/ChainOfResponsibility/demo/test.php
@@ -0,0 +1,121 @@
+<?php
+namespace App\Behavioral\ChainOfResponsibility\demo;
+
+abstract class AHandler
+
+{
+
+    protected $nextHandler = null;
+
+
+
+    public function setNext (AHandler $handler)
+
+    {
+
+        $this->nextHandler = $handler;
+
+    }
+
+
+
+    abstract public function handler ();
+
+}
+
+class Handler1 extends AHandler
+
+{
+
+    public function handler()
+
+    {
+
+        $handled = false;
+
+
+
+        // 处理请求
+
+        echo 'Handler1接受到了请求' . PHP_EOL;
+
+
+
+        if (!$handled && $this->nextHandler) {
+
+            $this->nextHandler->handler();
+
+        }
+
+    }
+
+}
+
+class Handler2 extends AHandler
+
+{
+
+    public function handler()
+
+    {
+
+        $handled = false;
+
+
+
+        // 处理请求
+
+        echo 'Handler2接受到了请求' . PHP_EOL;
+
+
+
+        if (!$handled && $this->nextHandler) {
+
+            $this->nextHandler->handler();
+
+        }
+
+    }
+
+}
+
+class Handler3 extends AHandler
+
+{
+
+    public function handler()
+
+    {
+
+        $handled = false;
+
+
+        // 处理请求
+
+        echo 'Handler3接受到了请求' . PHP_EOL;
+
+
+
+        if (!$handled && $this->nextHandler) {
+
+            $this->nextHandler->handler();
+
+        }
+
+    }
+
+}
+
+$handler1 = new Handler1();
+
+$handler2 = new Handler2();
+
+$handler3 = new Handler3();
+
+$handler1->setNext($handler2);
+
+$handler2->setNext($handler3);
+
+
+
+$handler1->handler();
